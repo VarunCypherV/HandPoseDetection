19edb7905a5d37b553aea9909b72bfcc31c88aba
VarunCypherV <varunvk2004@gmail.com>
2023-09-09 11:24:09 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import {drawhand} from "./utilities"
 
 function App() {
+  const webcamRef= useRef(null);
+  const canvasRef= useRef(null);
+
+  const runHandpose = async () =>{
+    const net = await handpose.load(); //waiting to model to load (neural network)
+    console.log("handpose loaded");
+    //loop and detect hand
+    //net is the neural network
+    setInterval(()=>{  //set interval js function , runs loop , 100 every 100 ms
+      detect(net);
+    },100)
+  };
+  const detect = async ( net) =>{
+        //check data if available
+        if(typeof webcamRef.current !== "undefined" 
+        && webcamRef.current !== null
+        && webcamRef.current.video.readyState === 4
+        ){
+          //set the ht and width
+          const video = webcamRef.current.video;
+          const videoWidth = webcamRef.current.video.videoWidth;
+          const videoHeight = webcamRef.current.video.videoHeight;
+
+          webcamRef.current.video.width = videoWidth;
+          webcamRef.current.video.height = videoHeight;
+
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
+
+          //making detection
+          const hand = await net.estimateHands(video);
+          console.log(hand); //landmarks r here see
+
+          //draw
+          const ctx=canvasRef.current.getContext('2d');
+          drawhand(hand, ctx);
+
+
+        }
+        
+        //make detection and draw mesh
+  }
+
+  runHandpose();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position:"absolute",
+            left:0,
+            right:0,
+            margin: "auto",
+            textAlign: "center",
+            zindex: 9,
+            height: 800,
+            width: 800,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position:"absolute",
+            left:0,
+            right:0,
+            margin: "auto",
+            textAlign: "center",
+            zindex: 9,
+            height: 800,
+            width: 800,
+          }}
+        />
       </header>
     </div>
   );
